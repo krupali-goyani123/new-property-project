7f6c857eb5fd57da3a2f80cde64a5899bfd2c265
krupali <krupaligoyani495@gmail.com>
2023-08-27 21:35:04 +0530

    add new nav-bar,property

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PropertyListComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule,
